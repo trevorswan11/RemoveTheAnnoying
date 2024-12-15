5cb1566571c946267a60400c1b415bc8b52db1db
Trevor Swan <tcs94@case.edu>
2024-12-15 02:44:30 -0500

    v1.1.0 Config!

--- RemoveTheAnnoying/Plugin.cs
+++ RemoveTheAnnoying/Plugin.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using RemoveTheAnnoying.Patches;
@@ -17,13 +18,16 @@ namespace RemoveTheAnnoying
     {
         private const string modGUID = "Kyoshi.RemoveAnnoyingStuff";
         private const string modName = "Remove Annoying Mechanics";
-        private const string modVersion = "1.0.3";
+        private const string modVersion = "1.1.0";
 
         private readonly Harmony harmony = new Harmony(modGUID);
-
         public static RemoveAnnoyingBase Instance;
-
         public static ManualLogSource mls;
+
+        public ConfigEntry<bool> MineshaftDisabled { get; private set; }
+        public ConfigEntry<bool> BarberDisabled { get; private set; }
+        public ConfigEntry<bool> ManeaterDisabled { get; private set; }
+        public ConfigEntry<bool> AllowFactoryArtifice { get; private set; }
 
         void Awake()
         {
@@ -31,6 +35,11 @@ namespace RemoveTheAnnoying
             {
                 Instance = this;
             }
+
+            MineshaftDisabled = Config.Bind<bool>("General", "DisableMineshaft", true, "Disables mineshaft interior when enabled.");
+            BarberDisabled = Config.Bind<bool>("General", "DisableBarber", true, "Disables all barber spawning when eneabled.");
+            ManeaterDisabled = Config.Bind<bool>("General", "DisableManeater", true, "Disables all maneater spawning when enabled.");
+            AllowFactoryArtifice = Config.Bind<bool>("General", "AllowArtificeFactory", true, "Allows factory interior on Artifice when enabled.");
 
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
             mls.LogInfo("Patching some QoL files!");
@@ -60,16 +69,23 @@ namespace RemoveTheAnnoying.Patches
     public class ChooseNewRandomMapSeedPatch
     {
         private static readonly ManualLogSource Logger = RemoveAnnoyingBase.mls;
+        private static readonly bool MineshaftDisabled = RemoveAnnoyingBase.Instance.MineshaftDisabled.Value;
+        private static readonly bool AllowFactoryArtifice = RemoveAnnoyingBase.Instance.AllowFactoryArtifice.Value;
+
         private const int MaxSeedAttempts = 1000;
         private const int MaxSeedValue = 100_000_000;
 
         // Postfix method - runs after round seed is generated
         private static void Postfix(StartOfRound __instance)
         {
+            // Can exit early if the Minesahft is enabled and Artfice is not banning factory
+            if (!MineshaftDisabled && AllowFactoryArtifice) return;
+
             // Get the seed and the level's manager and determine its type
             int randomSeed = __instance.randomMapSeed;
             RoundManager manager = RoundManager.Instance;
             InteriorType? type = DetermineType(randomSeed, manager);
+            string levelName = __instance.currentLevel.name.Replace("Level", "");
 
             // Check for The Company - I don't know if any of this is necessary
             if (ManagerIsCompany(manager) || StartOfRoundIsCompany(__instance))
@@ -84,6 +100,31 @@ namespace RemoveTheAnnoying.Patches
             type = type.Value;
             Logger.LogInfo($"Seed: {randomSeed} is a {type}.");
 
+            // Mineshaft disabled and artifice CAN spawn factory
+            if (MineshaftDisabled && AllowFactoryArtifice)
+            {
+                RemoveMineshaft(type, manager, randomSeed, __instance);
+            }
+
+            // Mineshaft disabled and artifice CANNOT spawn factory
+            else if (MineshaftDisabled && !AllowFactoryArtifice)
+            {
+                // Only force manor on artifice
+                if (levelName.Equals("Artifice")) ManorOnly(type, manager, randomSeed, __instance);
+                else RemoveMineshaft(type, manager, randomSeed, __instance);
+            }
+
+            // Mineshaft enabled and artifice cannot spawn factory
+            else if (!MineshaftDisabled && !AllowFactoryArtifice)
+            {
+                // Only block Factory on artifice
+                if (levelName.Equals("Artifice")) RemoveFactory(type, manager, randomSeed, __instance);
+            }
+        }
+
+        // Removes mineshaft generationa dn regenerates to find a new seed
+        private static void RemoveMineshaft(InteriorType? type, RoundManager manager, int randomSeed, StartOfRound __instance)
+        {
             // Check if the map is ok
             if (type.GetValueOrDefault() != InteriorType.Mineshaft)
             {
@@ -121,19 +162,86 @@ namespace RemoveTheAnnoying.Patches
             Logger.LogWarning("Regeneration failed after 1000 attempts");
         }
 
-        [HarmonyPatch(typeof(RoundManager), "GenerateNewFloor")]
-        public class GenerateNewFloorPatch
+        // Removes mineshaft generationa dn regenerates to find a new seed
+        private static void RemoveFactory(InteriorType? type, RoundManager manager, int randomSeed, StartOfRound __instance)
         {
-            // Prefix method - Runs before GenerateNewFloor does in the RoundManager class
-            private static bool Prefix(RoundManager __instance)
+            // Check if the map is ok
+            if (type.GetValueOrDefault() != InteriorType.Factory)
             {
-                // Modify the current level's dungeonFlowTypes by removing any entry where the id is the Mineshaft ID
-                __instance.currentLevel.dungeonFlowTypes = __instance.currentLevel.dungeonFlowTypes.Where(IsNotMineshaft).ToArray();
-                Logger.LogDebug($"Removed mineshaft generation of {__instance.currentLevel}.");
-                return true;
+                Logger.LogInfo("No need to regenerate seed.");
+                return;
             }
 
-            private static bool IsNotMineshaft(IntWithRarity flow) => flow.id != (int)InteriorType.Mineshaft;
+            // Otherwise the map must be a factory
+            Logger.LogInfo("Factory seed identified, trying to regenerate...");
+            manager.hasInitializedLevelRandomSeed = false;
+            manager.InitializeRandomNumberGenerators();
+
+            // Limit to 1000 total generation attempts
+            for (int i = 0; i < MaxSeedAttempts; i++)
+            {
+                randomSeed = NewSeed();
+                type = DetermineType((int)randomSeed, manager);
+                Logger.LogDebug($"Attempt {i + 1} - Seed: {randomSeed} Interior: {type}");
+
+                // Check for valid interior type
+                if (!type.HasValue)
+                {
+                    Logger.LogWarning("Detected unknown interior.");
+                    return;
+                }
+
+                // Check for mineshaft
+                if (new InteriorType?(type.Value).GetValueOrDefault() != InteriorType.Factory)
+                {
+                    __instance.randomMapSeed = randomSeed;
+                    Logger.LogInfo($"Generated new map seed: {randomSeed} after {i + 1} attempts.");
+                    return;
+                }
+            }
+            Logger.LogWarning("Regeneration failed after 1000 attempts");
+        }
+
+        // Removes both Factory and Mineshaft interior
+        private static void ManorOnly(InteriorType? type, RoundManager manager, int randomSeed, StartOfRound __instance)
+        {
+            // Check if the map is ok
+            if (type.GetValueOrDefault() != InteriorType.Mineshaft &&
+                type.GetValueOrDefault() != InteriorType.Factory)
+            {
+                Logger.LogInfo("No need to regenerate seed.");
+                return;
+            }
+
+            // Otherwise the map must be a mineshaft or factory
+            Logger.LogInfo("Mineshaft or Factory seed identified, trying to regenerate...");
+            manager.hasInitializedLevelRandomSeed = false;
+            manager.InitializeRandomNumberGenerators();
+
+            // Limit to 1000 total generation attempts
+            for (int i = 0; i < MaxSeedAttempts; i++)
+            {
+                randomSeed = NewSeed();
+                type = DetermineType((int)randomSeed, manager);
+                Logger.LogDebug($"Attempt {i + 1} - Seed: {randomSeed} Interior: {type}");
+
+                // Check for valid interior type
+                if (!type.HasValue)
+                {
+                    Logger.LogWarning("Detected unknown interior.");
+                    return;
+                }
+
+                // Check for mineshaft or factory generation
+                if (new InteriorType?(type.Value).GetValueOrDefault() != InteriorType.Mineshaft &&
+                    new InteriorType?(type.Value).GetValueOrDefault() != InteriorType.Factory)
+                {
+                    __instance.randomMapSeed = randomSeed;
+                    Logger.LogInfo($"Generated new map seed: {randomSeed} after {i + 1} attempts.");
+                    return;
+                }
+            }
+            Logger.LogWarning("Regeneration failed after 1000 attempts");
         }
 
         /// <summary>
@@ -203,6 +311,34 @@ namespace RemoveTheAnnoying.Patches
                 startOfRound.currentLevel.name.Equals("Gordion") ||
                 startOfRound.currentLevel.sceneName.Equals("CompanyBuilding");
         }
+
+        [HarmonyPatch(typeof(RoundManager), "GenerateNewFloor")]
+        public class GenerateNewFloorPatch
+        {
+            // Prefix method - Runs before GenerateNewFloor does in the RoundManager class
+            private static bool Prefix(RoundManager __instance)
+            {
+                string levelName = __instance.currentLevel.name.Replace("Level", "");
+
+                if (MineshaftDisabled)
+                {
+                    // Modify the current level's dungeonFlowTypes by removing any entry where the id is the Mineshaft ID
+                    __instance.currentLevel.dungeonFlowTypes = __instance.currentLevel.dungeonFlowTypes.Where(IsNotMineshaft).ToArray();
+                    Logger.LogDebug($"Removed mineshaft generation of {levelName}.");
+                }
+
+                if (levelName.Equals("Artifice") && !AllowFactoryArtifice)
+                {
+                    // Modify the current level's dungeonFlowTypes by removing any entry where the id is the Mineshaft ID
+                    __instance.currentLevel.dungeonFlowTypes = __instance.currentLevel.dungeonFlowTypes.Where(IsNotFactory).ToArray();
+                    Logger.LogDebug($"Removed factory generation of {levelName}.");
+                }
+                return true;
+            }
+
+            private static bool IsNotMineshaft(IntWithRarity flow) => flow.id != (int)InteriorType.Mineshaft;
+            private static bool IsNotFactory(IntWithRarity flow) => flow.id != (int)InteriorType.Factory;
+        }
     }
 
     [HarmonyPatch(typeof(RoundManager), "LoadNewLevel")]
@@ -211,10 +347,19 @@ namespace RemoveTheAnnoying.Patches
         private static readonly ManualLogSource Logger = RemoveAnnoyingBase.mls;
         private static readonly HashSet<string> DisabledEnemies 
             = new HashSet<string> { "ClaySurgeon", "CaveDweller" };
+        private static readonly bool BarberDisabled = RemoveAnnoyingBase.Instance.BarberDisabled.Value;
+        private static readonly bool ManeaterDisabled = RemoveAnnoyingBase.Instance.ManeaterDisabled.Value;
 
         // Prefix method - runs before the RoundManager loads the new level
         private static void Prefix(SelectableLevel newLevel)
         {
+            // Check if the user is ok with both enemies
+            if (!BarberDisabled && !ManeaterDisabled)
+            {
+                Logger.LogInfo("All unfun enemies allowed by user config.");
+                return;
+            }
+
             // Check if the level contains any of the disabled enemies
             if (!newLevel.Enemies.Any(e => DisabledEnemies.Contains(e.enemyType.name)))
             {
@@ -238,10 +383,25 @@ namespace RemoveTheAnnoying.Patches
         /// <param name="enemy">The desired enemy to alter.</param>
         private static bool DisableEnemyIfStinky(SpawnableEnemyWithRarity enemy)
         {
-            if (DisabledEnemies.Contains(enemy.enemyType.name))
+            string enemyName = enemy.enemyType.name;
+            if (DisabledEnemies.Contains(enemyName))
             {
+                // Check to see if the user is ok with the barber
+                if (enemyName.Equals("ClaySurgeon") && !BarberDisabled)
+                {
+                    Logger.LogInfo("Barber allowed due to user config.");
+                    return false;
+                }
+
+                // Check to see if the user is ok with the maneater
+                if (enemyName.Equals("CaveDweller") && !ManeaterDisabled)
+                {
+                    Logger.LogInfo("Maneater allowed due to user config");
+                    return false;
+                }
+
                 enemy.rarity = 0;
-                Logger.LogInfo($"Spawning of {enemy.enemyType.name} disabled.");
+                Logger.LogInfo($"Spawning of {enemyName} disabled.");
                 return true;
             }
             return false;
---
+++ Thunderstore/Releases/v1.1.0/CHANGELOG.md
@@ -0,0 +1,30 @@
+- **1.1.0**
+
+    - Added config options for disabling mineshaft and barber/maneater spawns
+        - You can now toggle the spawning of Barbers and Maneaters
+        - The mineshaft can be allowed if desired, but like why lol
+        - All Config options are independent of each other, they will not do anything silly if different combinations are used
+    - Added a new feature that prevents Facility generation on Artifice. This is a feature OFF BY DEFAULT, and can be toggled in the config
+    - Other minor refactoring
+    
+    *Note: Config options DO NOT update midgame and DO require a restart upon changing*
+
+    Thanks to [Jaexyr](https://github.com/Jaexyr) for the config suggestion!
+
+- **1.0.3**
+
+    - Debugging Fixes
+    - Code is marginally more readable
+    - This release is not necessary for anything plugin related, but is more debugging/efficiency focused
+
+- **1.0.2**
+
+    - Small fixes, nothing changed plugin wise
+
+- **1.0.1**
+
+    - Fixed an issue that prevented you from landing on The Company Building
+
+- **1.0.0**
+
+    - Mod Release
